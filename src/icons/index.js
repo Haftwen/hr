f6975d5d2934ed5d51e32ec3c6fa73885f13ee35
haftwen <1870470989@qq.com>
2022-09-24 10:51:16 +0800

    端口修改和标题的修改

--- src/icons/index.js
+++ src/icons/index.js
@@ -5,6 +5,7 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 Vue.component('svg-icon', SvgIcon)
 // webpack的批量导入
 const req = require.context('./svg', false, /\.svg$/)
+// 函数的作用是根据我传入的条件去匹配对应文件
 // req函数   后半部分写规则根据路径 找到对应的模块（目录，是否查找子文件，匹配的正则）
 // console.log(req)
 // const path = './dashboard.svg'
--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 // 通过axios.create()创建axios实例
 const service = axios.create({
-  baseUrl: '/dev',
+  baseUrl: process.env.VUE_APP_BASE_API,
   timeout: 5000
 })
 export default service
